9771546a9794d0feb8260ae3248202a2ae478dea
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2024-03-04 09:41:07 +0600

    ok

---  Backend/server.js
+++  Backend/server.js
@@ -1,26 +1,29 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 const cors = require("cors");
-const mongoose=require("mongoose");
-const routes=require('./routes')
+const mongoose = require("mongoose");
+const routes = require('./routes')
 
-mongoose.connect("mongodb+srv://Arnob:bonrA@techweb.6zzxtnh.mongodb.net/?retryWrites=true&w=majority",{dbName:'news-data'})
-.then(()=>{
-    app.listen(5000,()=>{
-        console.log("Success");
+mongoose.connect("mongodb+srv://Arnob:bonrA@techweb.6zzxtnh.mongodb.net/?retryWrites=true&w=majority", { dbName: 'news-data' })
+    .then(() => {
+        app.listen(5000, () => {
+            console.log("Success");
+        })
     })
-})
-.catch((err)=>{
-    console.log(err);
-})
-    // app.listen(5000,()=>{
-    //     console.log("Success");
-    // })
-
+    .catch((err) => {
+        console.log(err);
+    })
+// app.listen(5000,()=>{
+//     console.log("Success");
+// })
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow access from this origin
+};
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(cors());
-app.use('/jobs',routes)
-app.use('/',routes)
+app.use('/jobs', routes)
+app.use('/', routes)
 
 
 app.use('/images', express.static('images'));
